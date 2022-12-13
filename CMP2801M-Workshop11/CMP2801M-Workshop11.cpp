b27b4ceb221d6cbc3bb9ceabe090b455dedf9d12
hazzakak <smith.harry1807@hotmail.co.uk>
2022-12-13 15:07:40 +0000

    Add project files.

---
+++ CMP2801M-Workshop11/CMP2801M-Workshop11.cpp
@@ -0,0 +1,9 @@
+// CMP2801M-Workshop11.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
